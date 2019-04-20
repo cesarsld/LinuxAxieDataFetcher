926dee8a78be6dcd6856d4784ab8a79c84f5c33d
cesarsld <jaimes.cesar88@gmail.com>
2019-04-20 09:30:50 +0100

    Add project files.

---
+++ LinuxAxieAnalytics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinuxAxieAnalytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
